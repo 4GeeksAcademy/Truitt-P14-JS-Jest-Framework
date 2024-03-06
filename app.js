f2ac43ee7db03f280c4739c497b91a0f372e5b5a
Truitt Janney <153782672+truittjanney@users.noreply.github.com>
2024-03-06 14:57:11 +0000

    project progress

---
+++ app.js
@@ -0,0 +1,24 @@
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+}
+
+const fromdollarToYen = function(valueInDollar) {
+    let valueInYen = valueInEuro * 156.5;
+    return valueInYen;
+}
+
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const sum = (a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
